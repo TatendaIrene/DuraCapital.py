9ad482b6aefa7fc58b9afae9f333a5f1a81b2359
TatendaIrene <107921883+TatendaIrene@users.noreply.github.com>
2023-05-27 01:21:35 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,32 +1,44 @@
-def count_vehicles(number_plate):
-    if len(number_plate) != 7:  # Check if the number plate has 7 characters
-        return None, None  # Return None values if the number plate is invalid
+# Registered vehicle counter
 
-    letters = number_plate[:3].upper()  # Extract the first 3 characters (letters) and convert them to uppercase
-    numbers = number_plate[3:]  # Extract the remaining characters (numbers)
+# Check if the number plate has 7 characters
+def count_vehicles(plate_number):
+    if len(plate_number) != 7:
+        # Return none values if the number plate is invalid
+        return None, None
 
-    if not letters.isalpha() or not numbers.isdigit():  # Check if the letters are alphabetic and numbers are digits
-        return None, None  # Return None values if the number plate is invalid
+# Extract the first 3 characters (letters) and convert them to uppercase
+    plate_letters = number_plate[:3].upper()
+    # Extract the remaining characters (numbers)
+    plate_numbers = number_plate[3:]
 
-    # Calculate the count based on the letters and numbers in the number plate
-    count = (ord(letters[0]) - ord('A')) * 26 * 26
-    count += (ord(letters[1]) - ord('A')) * 26
-    count += ord(letters[2]) - ord('A')
+# Check if the letters are alphabetic and numbers are digits
+    if not plate_letters.isalpha() or not plate_numbers.isdigit():
+        # Return none values if the number plate is invalid
+        return None, None
+
+# Calculate the count based on the letters and number plates
+    count = (ord(plate_letters[0]) - ord('A')) * 26 * 26
+    count += (ord(plate_letters[1]) - ord('A')) * 26
+    count += ord(plate_letters[2]) - ord('A')
     count *= 10000
-    count += int(numbers)
+    count += int(plate_numbers)
 
-    return count, count - 1  # Return the count and previous count
+# Return the count and the previous count
+    return count, count - 1
 
 
-number_plate = input("Enter the number plate: ")  # Prompt the user to enter a number plate
-count, previous_count = count_vehicles(number_plate)  # Call the count_vehicles function with the provided number plate
+# Prompt the user to enter a number plate
+number_plate = input("Enter the number plate: ")
+# Call the count_vehicles function with the given number plate
+plate_count, previous_count = count_vehicles(number_plate)
 
-if count is None:  # Check if the count is None, indicating an invalid number plate
-    print("Invalid number plate. Please enter a valid number plate.")
+# Check if the count is none, indicating an invalid number plate
+if plate_count is None:
+    print("The number plate is invalid. Please enter a valid number plate.")
 else:
-    if count == 1:
-        # Print the information for the first car
-        print(f"The car with number plate {number_plate} is the first car, and 0 cars were registered before the car.")
+    # Print the information for the first car
+    if plate_count == 1:
+        print(f"Car with number plate {number_plate} is the first car, and 0 cars were registered before the car.")
     else:
-        # Print the information for a car other than the first car
-        print(f"The car with number plate {number_plate} is the {count}th car and {previous_count} cars were registered before the car.")
+        print(f"Car with number plate {number_plate} is the {plate_count}th car and {previous_count} cars were "
+              f"registered before the car.")
